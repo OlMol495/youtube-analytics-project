086c0702a31c1e7eeb1898745174794217e53809
Olga Lebedeva <olga.s.lebedeva@outlook.com>
2023-10-13 19:56:01 +0900

    minor changes to Channel methods

--- src/channel.py
+++ src/channel.py
@@ -9,13 +9,13 @@ class Channel:
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.__channel_id = channel_id
-        dict_to_load = self.get_service().channels().list(id=self.__channel_id, part='snippet,statistics').execute()
-        self.title = dict_to_load["items"][0]["snippet"]["title"]
-        self.description = dict_to_load["items"][0]["snippet"]["description"]
-        self.url = dict_to_load["items"][0]["snippet"]["thumbnails"]["default"]["url"]
-        self.subscriber_count = dict_to_load["items"][0]["statistics"]["subscriberCount"]
-        self.video_count = dict_to_load["items"][0]["statistics"]["videoCount"]
-        self.viewing_count = dict_to_load["items"][0]["statistics"]["viewCount"]
+        self.dict_to_load = self.get_service().channels().list(id=self.__channel_id, part='snippet,statistics').execute()
+        self.title = self.dict_to_load["items"][0]["snippet"]["title"]
+        self.description = self.dict_to_load["items"][0]["snippet"]["description"]
+        self.url = self.dict_to_load["items"][0]["snippet"]["thumbnails"]["default"]["url"]
+        self.subscriber_count = self.dict_to_load["items"][0]["statistics"]["subscriberCount"]
+        self.video_count = self.dict_to_load["items"][0]["statistics"]["videoCount"]
+        self.viewing_count = self.dict_to_load["items"][0]["statistics"]["viewCount"]
 
     def __str__(self):
         return f"{self.title} ({self.url}"
@@ -34,15 +34,13 @@ class Channel:
 
     def to_json(self, json_file):
         """download to json file"""
-        dict_to_load = self.get_service().channels().list(id=self.__channel_id, part='snippet,statistics').execute()
         with open(json_file, "w") as f:
-            json.dump(dict_to_load, f)
+            json.dump(self.dict_to_load, f)
 
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        dict_to_print = self.get_service().channels().list(id=self.__channel_id, part='snippet,statistics').execute()
-        print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
+        print(json.dumps(self.dict_to_load, indent=2, ensure_ascii=False))
 
     def __add__(self, other):
         return self.subscriber_count + other.subscriber_count
